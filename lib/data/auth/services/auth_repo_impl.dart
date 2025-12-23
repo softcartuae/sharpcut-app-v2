eeebe350931b7dda8129b3fcb42c6a827facc063
yasirpk <pkyasir067@gmail.com>
2025-12-23 13:00:54 +0530

    created a booking model class

--- lib/data/auth/services/auth_repo_impl.dart
+++ lib/data/auth/services/auth_repo_impl.dart
@@ -24,18 +24,7 @@ class AuthRepoImpl implements AuthRepo {
   @override
   Future<UserModel> getUser() async {
     try {
-      // Note: Token should be set in Dio headers before calling this,
-      // or passed here if we were managing it differently.
-      // For now, assuming the interceptor or global header is set elsewhere
-      // or we need to set it.
-      // Since the user didn't specify where the token is stored,
-      // I will assume for now we might need to add it to the headers dynamically
-      // if I had a way to get it.
-      // However, typically the AuthCubit will hold the token and we might need
-      // to pass it or configure Dio.
-      // Let's assume for this step that the token is handled or we will handle it in Cubit
-      // by setting ApiClient.dio.options.headers["Authorization"] = "Bearer $token";
-
+  
       final response = await ApiClient.dio.get(ApiClient.authentcatedUserApi);
 
       if (response.statusCode == 200) {
