98c2f641f0aaae4a3b29545c5fd8e42ca6d346a4
yasirpk <pkyasir067@gmail.com>
2026-01-31 16:32:07 +0530

    added discout

--- lib/presentation/report/widgets/report_data_table.dart
+++ lib/presentation/report/widgets/report_data_table.dart
@@ -161,7 +161,7 @@ class _ReportDataTableState extends State<ReportDataTable> {
                                                 subTotalValue:
                                                     booking.subtotal ?? 0,
                                                 taxTotal: booking.taxTotal ?? 0,
-                                                discount: 0.0,
+                                                discount: booking.discount ?? 0,
                                                 roundOff: 0.0,
                                                 finalTotal:
                                                     booking.finalTotal ?? 0,
