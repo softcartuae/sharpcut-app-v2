7fa42f149a6c21aa6f42b775e14e440e3ca3fffa
yasirpk <pkyasir067@gmail.com>
2026-02-10 18:25:56 +0530

    changed synce payload

--- lib/presentation/home/widgets/home_appbar.dart
+++ lib/presentation/home/widgets/home_appbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sharp_cut/cubit/auth/auth_cubit.dart';
+import 'package:sharp_cut/presentation/test/database_viewer_screen.dart';
 
 import 'package:sharp_cut/utils/app_colors.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -39,6 +40,13 @@ class _HomeAppBarState extends State<HomeAppBar> {
         Row(
           children: [
             GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const DatabaseViewerScreen(),
+                  ),
+                );
+              },
               onLongPress: () {
                 final isNoChair = CheckNoChair.checkIsThisAppNoChairOrNot(
                   context.read<AuthCubit>().currentUser,
