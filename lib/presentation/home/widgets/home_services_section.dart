8bdc668e4e363705f3c1fc53bf541df15c0ca40c
yasirpk <pkyasir067@gmail.com>
2026-02-13 16:24:03 +0530

    online build with staging url

--- lib/presentation/home/widgets/home_services_section.dart
+++ lib/presentation/home/widgets/home_services_section.dart
@@ -350,6 +350,7 @@ class _HomeServicesSectionState extends State<HomeServicesSection> {
                           state.response.bookingResponse?.finalTotalbefore,
                     );
 
+
                 printCubit.printInvoice(
                   chairId: _pendingPrintData!.chairId,
                   printCount: printCubit.state.settings?.printCount.quickPayment
