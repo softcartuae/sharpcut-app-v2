ca68091d2b3015995172386ac8ac3100702451ce
yasirpk <pkyasir067@gmail.com>
2026-03-27 18:58:07 +0530

    online windows

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ void main() async {
     Bloc.observer = SimpleBlocObserver();
     runApp(
       DevicePreview(
-        enabled: false,
+        enabled: true,
         builder: (context) => MultiBlocProvider(
           providers: [
             BlocProvider(create: (_) => di.sl<ServiceCubit>()),
