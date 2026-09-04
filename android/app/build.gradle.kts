59944f0ee67a6e6e066eb8c8eb2b05e17a3347a5
yasirpk <pkyasir067@gmail.com>
2026-09-04 18:33:18 +0530

    push notiffication

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,6 +23,7 @@ android {
 
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -64,4 +65,8 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
